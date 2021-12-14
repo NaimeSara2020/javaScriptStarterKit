5860188f3c35cf4dfbf859618a2306952fdf2a60
NaimeSara2020 <naimesara95@gmail.com>
2021-12-14 16:57:38 +0300

    18 Ekim ders sonu

--- src/app.js
+++ src/app.js
@@ -1,36 +1,21 @@
-console.log("Merhaba Kodlama.io")
+console.log("Merhaba Kodlama.io");
 
-//JS type safe değildir
-let dolarBugun = 9.30
-
-let dolarDun = 9.20
-dolarDun = "9.20"
+//js type safe değildir.
+let dolarDun = 9.20;
+let dolarBugun = 9.30;
 {
-    let dolarDun = 9.10
+    let dolarDun = 9.10;
 }
-
-console.log(dolarDun)
-
-const euroDun = 11.2
-//euroDun = 11
-
-console.log(euroDun)
-
+const euroDun = 11.2;
 //array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
-
+//camelCasing kelimenin ilk harfi küçük ikinci harf büyük
+//PascalCasing kelimlerin ilk harfleri büyük
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredileri"];
+//react
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
+for (let i = 0; i < konutKredileri.length; i++) {
+     console.log("<li>"+konutKredileri[i]+"</li>");
 }
 console.log("</ul>")
-
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
-
-console.log(konutKredileri)
+//ctrl+k+c kopyalama
+console.log(konutKredileri);
